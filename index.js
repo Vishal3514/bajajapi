630f470b714982d1bb19a165240f5c497583dd9d
Vishal Kumar <84696166+Vishal3514@users.noreply.github.com>
2022-03-02 14:18:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+app.post('/bfhl',(req,res)=>{
+    let status = true;
+    try{
+    let num_Arr = []
+    let alf_Arr = []
+    let arr = req.body.data
+    for(let i=0;i<arr.length;i++){
+        if(!isNaN(arr[i])){
+            num_Arr.push(arr[i])
+        }else{
+            alf_Arr.push(arr[i])
+        }
+    }
+    let ans = {
+        is_success: status,
+        user_id: "vishal_kumar_11012001",
+        email: "vishalkumar.cse19@chitkarauniversity.edu.in",
+        roll_number: "1911981041",
+        numbers:num_Arr,
+        alphabets:alf_Arr
+    }
+    res.send(ans)
+    }
+    catch(err){
+        status = false;
+        res.send({
+            is_success: status
+        })
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running at port 8000");
+});
